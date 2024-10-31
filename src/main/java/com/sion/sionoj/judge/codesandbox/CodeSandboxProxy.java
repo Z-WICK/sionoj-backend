40b2adff4cbd3e730c5acf4faa1d6d3ce3b66c8c
Z-WICK <71963565+Z-WICK@users.noreply.github.com>
2024-10-31 19:40:44 +0800

    feat: 代理模式优化

--- src/main/java/com/sion/sionoj/judge/codesandbox/CodeSandboxProxy.java
+++ src/main/java/com/sion/sionoj/judge/codesandbox/CodeSandboxProxy.java
@@ -4,19 +4,15 @@ import com.sion.sionoj.judge.codesandbox.model.ExecuteCodeRequest;
 import com.sion.sionoj.judge.codesandbox.model.ExecuteCodeResponse;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * @Author : wick
- * @Date : 2024/10/30 20:32
- */
 @Slf4j
-public class CodeSandboxProxy implements CodeSandbox{
+public class CodeSandboxProxy implements CodeSandbox {
 
     private final CodeSandbox codeSandbox;
 
-      public CodeSandboxProxy(CodeSandbox codeSandbox) {
+
+    public CodeSandboxProxy(CodeSandbox codeSandbox) {
         this.codeSandbox = codeSandbox;
     }
-
 
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
--- src/test/java/com/sion/sionoj/judge/codesandbox/CodeSandboxTest.java
+++ src/test/java/com/sion/sionoj/judge/codesandbox/CodeSandboxTest.java
@@ -6,6 +6,7 @@ import com.sion.sionoj.judge.codesandbox.model.ExecuteCodeResponse;
 import com.sion.sionoj.model.enums.QuestionSubmitLanguageEnum;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
@@ -14,6 +15,9 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class CodeSandboxTest {
+
+    @Value("${codesandbox.type:example}")
+    private String type;
 
     @Test
     void executeCode() {
@@ -28,6 +32,37 @@ class CodeSandboxTest {
                 .build();
 
         ExecuteCodeResponse executeCodeResponse = exampleCodeSandbox.executeCode(executeCodeRequest);
+        Assertions.assertNotNull(executeCodeResponse);
+    }
+
+    @Test
+    void executeCodeByValue() {
+        CodeSandbox codeSandbox = CodeSandboxFactory.newInstance(type);
+        String code = "int main() { }";
+        String language = QuestionSubmitLanguageEnum.JAVA.getValue();
+        List<String> inputList = Arrays.asList("1 2", "3 4");
+        ExecuteCodeRequest executeCodeRequest = ExecuteCodeRequest.builder()
+                .code(code)
+                .language(language)
+                .inputList(inputList)
+                .build();
+        ExecuteCodeResponse executeCodeResponse = codeSandbox.executeCode(executeCodeRequest);
+        Assertions.assertNotNull(executeCodeResponse);
+    }
+
+    @Test
+    void executeCodeByProxy() {
+        CodeSandbox codeSandbox = CodeSandboxFactory.newInstance(type);
+        codeSandbox = new CodeSandboxProxy(codeSandbox);
+        String code = "int main() { }";
+        String language = QuestionSubmitLanguageEnum.JAVA.getValue();
+        List<String> inputList = Arrays.asList("1 2", "3 4");
+        ExecuteCodeRequest executeCodeRequest = ExecuteCodeRequest.builder()
+                .code(code)
+                .language(language)
+                .inputList(inputList)
+                .build();
+        ExecuteCodeResponse executeCodeResponse = codeSandbox.executeCode(executeCodeRequest);
         Assertions.assertNotNull(executeCodeResponse);
     }
 }
