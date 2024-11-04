ef685544309b9b3285bb567b9f85e9637c37d689
Z-WICK <2289431216@qq.com>
2024-11-04 20:20:54 +0800

    test: 远程代码沙箱对接测试

--- src/main/java/com/sion/sionoj/judge/codesandbox/impl/RemoteCodeSandbox.java
+++ src/main/java/com/sion/sionoj/judge/codesandbox/impl/RemoteCodeSandbox.java
@@ -1,8 +1,15 @@
 package com.sion.sionoj.judge.codesandbox.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.sion.sionoj.common.ErrorCode;
+import com.sion.sionoj.exception.BusinessException;
 import com.sion.sionoj.judge.codesandbox.CodeSandbox;
 import com.sion.sionoj.judge.codesandbox.model.ExecuteCodeRequest;
 import com.sion.sionoj.judge.codesandbox.model.ExecuteCodeResponse;
+import freemarker.template.utility.StringUtil;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @Author : wick
@@ -15,7 +22,18 @@ public class RemoteCodeSandbox implements CodeSandbox {
 
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
-        System.out.println("远程代码沙箱执行代码");
-        return null;
+        System.out.println("远程代码沙箱");
+        String url = "http://localhost:8090/executeCode";
+        String json = JSONUtil.toJsonStr(executeCodeRequest);
+        String responseStr = HttpUtil.createPost(url)
+                .body(json)
+                .execute()
+                .body();
+
+        if (StrUtil.isBlank(responseStr)){
+            throw new BusinessException(ErrorCode.API_REQUEST_ERROR,"executeCode remoteSandbox error, message =" + responseStr);
+        }
+        return JSONUtil.toBean(responseStr,ExecuteCodeResponse.class);
+
     }
 }
