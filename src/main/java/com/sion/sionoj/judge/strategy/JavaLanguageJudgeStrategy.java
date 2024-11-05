c62f9f985f81c1e04200b3dcd5bdb77a64e7e8fd
Z-WICK <2289431216@qq.com>
2024-11-05 11:33:53 +0800

    fix : 异常处理memory time 值为空

--- src/main/java/com/sion/sionoj/judge/strategy/JavaLanguageJudgeStrategy.java
+++ src/main/java/com/sion/sionoj/judge/strategy/JavaLanguageJudgeStrategy.java
@@ -1,13 +1,14 @@
 package com.sion.sionoj.judge.strategy;
 
 import cn.hutool.json.JSONUtil;
+import com.sion.sionoj.judge.codesandbox.model.JudgeInfo;
 import com.sion.sionoj.model.dto.question.JudgeCase;
 import com.sion.sionoj.model.dto.question.JudgeConfig;
-import com.sion.sionoj.judge.codesandbox.model.JudgeInfo;
 import com.sion.sionoj.model.entity.Question;
 import com.sion.sionoj.model.enums.JudgeInfoMessageEnum;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @Author : wick
@@ -18,15 +19,17 @@ public class JavaLanguageJudgeStrategy implements JudgeStrategy {
 
     /**
      * 执行判题
+     *
      * @param judgeContext
      * @return {@link JudgeInfo }
      */
     @Override
     public JudgeInfo doJudge(JudgeContext judgeContext) {
         JudgeInfo judgeInfo = judgeContext.getJudgeInfo();
-        Long memory = judgeInfo.getMemory();
-        Long time = judgeInfo.getTime();
-        List<String> inputList  = judgeContext.getInputList();
+        //todo 修正memory 与 time 为空
+        Long memory = Optional.of(judgeInfo.getMemory()).orElse(0L);
+        Long time = Optional.of(judgeInfo.getTime()).orElse(0L);
+        List<String> inputList = judgeContext.getInputList();
         List<String> outputList = judgeContext.getOutputList();
         Question question = judgeContext.getQuestion();
         List<JudgeCase> judgeCaseList = judgeContext.getJudgeCaseList();
@@ -46,7 +49,7 @@ public class JavaLanguageJudgeStrategy implements JudgeStrategy {
         //依次判断每一项输出和预期输出是否相等
         for (int i = 0; i < judgeCaseList.size(); i++) {
             JudgeCase judgeCase = judgeCaseList.get(i);
-            if(!judgeCase.getOutput().equals(outputList.get(i))){
+            if (!judgeCase.getOutput().equals(outputList.get(i))) {
                 judgeInfoMessageEnum = JudgeInfoMessageEnum.WRONG_ANSWER;
                 judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
                 return judgeInfoResponse;
@@ -59,17 +62,17 @@ public class JavaLanguageJudgeStrategy implements JudgeStrategy {
         Long needMemoryLimit = judgeConfig.getMemoryLimit();
         Long needTimeLimit = judgeConfig.getTimeLimit();
 
-        if(memory>needMemoryLimit){
-           judgeInfoMessageEnum =  JudgeInfoMessageEnum.MEMORY_LIMIT_EXCEEDED;
-           judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
-           return judgeInfoResponse;
+        if (memory > needMemoryLimit) {
+            judgeInfoMessageEnum = JudgeInfoMessageEnum.MEMORY_LIMIT_EXCEEDED;
+            judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+            return judgeInfoResponse;
         }
         //Java 程序本身需要额外执行10 秒钟
         long JAVA_PROGRAM_TIME_COST = 10000L;
-        if((time - JAVA_PROGRAM_TIME_COST) > needTimeLimit){
-           judgeInfoMessageEnum =  JudgeInfoMessageEnum.TIME_LIMIT_EXCEEDED;
-           judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
-           return judgeInfoResponse;
+        if ((time - JAVA_PROGRAM_TIME_COST) > needTimeLimit) {
+            judgeInfoMessageEnum = JudgeInfoMessageEnum.TIME_LIMIT_EXCEEDED;
+            judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
+            return judgeInfoResponse;
         }
         judgeInfoResponse.setMessage(judgeInfoMessageEnum.getValue());
         return judgeInfoResponse;
