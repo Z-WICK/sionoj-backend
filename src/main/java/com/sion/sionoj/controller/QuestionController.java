dc5fe803024850dfe19002308a06789125bc273f
Z-WICK <2289431216@qq.com>
2024-11-05 11:36:17 +0800

    refactor: 合并接口
    为了保证后续微服务拆分-稳定

--- src/main/java/com/sion/sionoj/controller/QuestionController.java
+++ src/main/java/com/sion/sionoj/controller/QuestionController.java
@@ -13,10 +13,15 @@ import com.sion.sionoj.exception.BusinessException;
 import com.sion.sionoj.exception.ThrowUtils;
 import com.sion.sionoj.model.dto.question.JudgeConfig;
 import com.sion.sionoj.model.dto.question.*;
+import com.sion.sionoj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.sion.sionoj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
 import com.sion.sionoj.model.entity.Question;
+import com.sion.sionoj.model.entity.QuestionSubmit;
 import com.sion.sionoj.model.entity.User;
+import com.sion.sionoj.model.vo.QuestionSubmitVO;
 import com.sion.sionoj.model.vo.QuestionVO;
 import com.sion.sionoj.service.QuestionService;
+import com.sion.sionoj.service.QuestionSubmitService;
 import com.sion.sionoj.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -36,12 +41,14 @@ import java.util.List;
 public class QuestionController {
 
     @Resource
+    private QuestionSubmitService questionSubmitService;
+
+    @Resource
     private QuestionService questionService;
 
     @Resource
     private UserService userService;
-    @Autowired
-    private Gson gson;
+
 
     // region 增删改查
 
@@ -287,6 +294,43 @@ public class QuestionController {
         }
         boolean result = questionService.updateById(question);
         return ResultUtils.success(result);
+    }
+
+    /**
+     * 提交题目
+     *
+     * @param questionSubmitAddRequest
+     * @param request
+     * @return 提交记录的ID
+     */
+    @PostMapping("/question_submit/do")
+    public BaseResponse<Long> doQuestionSubmit(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
+            HttpServletRequest request) {
+        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long questionSubmitId = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
+        return ResultUtils.success(questionSubmitId);
+    }
+
+    /**
+     * 分页获取列表（除管理员外, 普通用户只能看到非答案、提交代码等公开信息）
+     *
+     * @param questionSubmitQueryRequest
+     * @return
+     */
+    @PostMapping("/question_submit/list/page")
+    public BaseResponse<Page<QuestionSubmitVO>> listQuestionByPage(@RequestBody QuestionSubmitQueryRequest questionSubmitQueryRequest, HttpServletRequest request) {
+        long current = questionSubmitQueryRequest.getCurrent();
+        long size = questionSubmitQueryRequest.getPageSize();
+        //从数据库中查询原始的题目提交分页信息
+        Page<QuestionSubmit> questionSubmitPage = questionSubmitService.page(new Page<>(current, size),
+                questionSubmitService.getQueryWrapper(questionSubmitQueryRequest));
+        final User loginUser = userService.getLoginUser(request);
+        //返回脱敏信息
+        return ResultUtils.success(questionSubmitService.getQuestionSubmitVOPage(questionSubmitPage,loginUser));
     }
 
 }
